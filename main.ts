20dd4cff2c2d8b8d3c9cdfc154c184cb127ac9f9
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-05-20 16:38:16 +1000

    Transfer work to other machine

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
-import { Plugin, WorkspaceLeaf } from "obsidian";
+import { Plugin } from "obsidian";
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
-};
+// interface MyPluginSettings {
+// 	mySetting: string;
+// }
+//
+// const DEFAULT_SETTINGS: MyPluginSettings = {
+// 	mySetting: "default",
+// };
 
 export default class PinEnhancerPlugin extends Plugin {
-	settings: MyPluginSettings;
+	// settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
+		// await this.loadSettings();
+
 		console.log("loading PinEnhancerPlugin");
+
 		this.handleLayoutChange();
 	}
 
@@ -21,24 +23,57 @@ export default class PinEnhancerPlugin extends Plugin {
 		console.log("unloading PinEnhancerPlugin");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData(),
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	// async loadSettings() {
+	// 	this.settings = Object.assign(
+	// 		{},
+	// 		DEFAULT_SETTINGS,
+	// 		await this.loadData(),
+	// 	);
+	// }
+	//
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
 
 	handleLayoutChange() {
 		const leaves = this.app.workspace.getLeavesOfType("markdown");
 		leaves.forEach((leaf) => {
 			if (leaf.getViewState().pinned) {
-				console.log("Pinned leaf", leaf);
+				// TODO: Prevent closing
+				this.preventClose();
 			}
 		});
+	}
+
+	preventClose() {
+		const tabsContainer = document.querySelectorAll(
+			".workspace-tab-header-container-inner",
+		)[1];
+
+		tabsContainer
+			.querySelectorAll(".workspace-tab-header")
+			.forEach((tab) => {
+				const statusIcon = tab.querySelector(
+					".workspace-tab-header-status-icon",
+				);
+
+				if (statusIcon && statusIcon.hasClass("mod-pinned")) {
+					// Prevent middle-mouse close
+					tab.addEventListener("mousedown", (e) => {
+						this.blockMouseAction(e);
+						console.log("Preventing close");
+					});
+
+					// TODO: Context menu closes
+
+					// TODO: Command palette closes
+				}
+			});
+	}
+
+	blockMouseAction(event: Event) {
+		event.stopPropagation();
+		event.preventDefault();
+		console.log("Mouse event blocked");
 	}
 }
